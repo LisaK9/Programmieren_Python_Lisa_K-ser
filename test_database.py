43acbb339cbf6bf422794298ae4f7b09b8cdb566
lisa0908 <108758315+lisa0908@users.noreply.github.com>
2023-01-08 16:49:09 +0100

    Hinzufügen eines test, der prüft, ob das Exceptionhandling richtig funktioniert

--- test_database.py
+++ test_database.py
@@ -1,8 +1,27 @@
 import database
 import pytest
 
+"""test if readCsv works correctly"""
+
 
 def test_readCsv():
     csv = database.ReadCsv()
     csv.readCsv('test.csv')
     assert csv.getHeader() == ['x', 'y']
+
+
+"""test if Exception handling for train and ideal works correctly"""
+
+
+@pytest.mark.parametrize('files',
+                         ['test.csv', 'ideal.csv', 'train.csv', 'test_.csv', 'example.csv'])
+def test_readCsv(files):
+    csv = database.DatabaseOperations('table_name')
+    try:
+        csv.readCsv(files)
+    except FileNotFoundError as e:
+        print(e)
+    except Exception as e:
+        print('Error: ', e)
+    else:
+        print(csv.getHeader())
