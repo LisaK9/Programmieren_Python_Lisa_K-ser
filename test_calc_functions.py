726af99593673b95608ec81718bfc8b3c5637be6
lisa0908 <108758315+lisa0908@users.noreply.github.com>
2023-01-08 17:07:46 +0100

    Test für FUnktion find_ideal

--- test_calc_functions.py
+++ test_calc_functions.py
@@ -9,3 +9,16 @@ import pytest
                           (1.75, -7.9, 93.1225), (-5.111, -4.45, 0.43692099999999945)])
 def test_least_squared(input1, input2, expected):
     assert calc_functions.least_squared(input1, input2) == expected
+
+
+"""test if the function find_ideal works correctly"""
+
+
+def test_find_ideal():
+    sum_abw = [7, 2, 5, 4, 5, 1, 7, 8, 9]
+    min_abw = 1
+    result = calc_functions.find_ideal(sum_abw, min_abw)
+    assert result[5] == "Yes"
+    assert result[1] == "No"
+    assert result[4] == "No"
+    assert result[0] == "No"
