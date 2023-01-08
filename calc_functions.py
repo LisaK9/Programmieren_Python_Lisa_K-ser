fd7257af9937b117b233d8e7843eaae7218dfd78
lisa0908 <108758315+lisa0908@users.noreply.github.com>
2023-01-08 16:57:39 +0100

    Datei calc_functions. Funktion zur berechnung der quadratischen y-Abweichung wurde hinzugefügt

---
+++ calc_functions.py
@@ -0,0 +1,9 @@
+
+
+""" Function to calculate the least squared y deviation.
+  Input: y, y_ideal
+  Output: least squared y deviation"""
+
+
+def least_squared(y, y_ideal):
+    return (y - y_ideal) ** 2
