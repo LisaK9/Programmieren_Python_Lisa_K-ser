44d95dc05175ad50a385a7f54f08f69a316a0f51
lisa0908 <108758315+lisa0908@users.noreply.github.com>
2023-01-08 16:46:09 +0100

    Csv Dateien ideal und train mittels Exception Handling öffnen. Fall die Datei nicht gefunden wird, wird ein FileNotFoundError ausgegeben. Anschließend werden alle andere Errors abgefangen und jeweils die spezifische Meldung ausgegeben. Falls kein Fehler auftritt, werden die Spaltennamen der CSV Datei ausgegeben, die Datenbanktabelle gelöscht, angelegt und die Daten in die Datenbanktabelle geschrieben, sowie die Daten wieder aus der Datenbanktabelle gelesen und als Dataframe zurückgegeben

--- main.py
+++ main.py
@@ -1,9 +1,38 @@
 import database
 
-
 # Read train.csv and ideal.csv, create table and write data into DB
 
 # create new object and instance of class DatabaseOperations with table name train
 data_train = database.DatabaseOperations('TRAIN')
 # create new object and instance of class DatabaseOperations with table name ideal
 data_ideal = database.DatabaseOperations('IDEAL')
+
+# Exception Handling, try to open the csv file. If FileNotFoundError print this Error. For other Errors print Error.
+# Else print Header of csv file and drop table, create table and insert the data into the table
+try:
+    data_train.readCsv('train.csv')  # open csv file
+except FileNotFoundError as e:
+    print("File not found: ", e)
+except Exception as e:
+    print('Error: ', e)
+else:
+    print("Header of CSV file: ", data_train.getHeader())  # get header of csv file
+    data_train.dropTable()  # drop table train
+    data_train.createTable()  # create table train
+    data_train.insertTable()  # insert data into table train
+    data_train = data_train.getQuery()  # return sql data from table train as a dataframe
+
+# Read ideal csv, create table and write data into DB
+
+try:
+    data_ideal.readCsv('ideal.csv')  # open csv file
+except FileNotFoundError as e:
+    print(e)
+except Exception as e:
+    print('Error: ', e)
+else:
+    print("Header of CSV file: ", data_ideal.getHeader())  # get header of csv file
+    data_ideal.dropTable()  # drop table ideal
+    data_ideal.createTable()  # create table ideal
+    data_ideal.insertTable()  # insert data into table ideal
+    data_ideal = data_ideal.getQuery()  # return sql data from table train as dataframe
