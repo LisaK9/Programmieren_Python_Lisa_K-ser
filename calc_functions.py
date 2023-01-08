903a9af7f9a062b658348ec12b07c76d57b51713
lisa0908 <108758315+lisa0908@users.noreply.github.com>
2023-01-08 17:04:24 +0100

    FUnktion find_ideal, mit der die ideale FUnktion gefunden werden kann

--- calc_functions.py
+++ calc_functions.py
@@ -7,3 +7,18 @@
 
 def least_squared(y, y_ideal):
     return (y - y_ideal) ** 2
+
+
+""" Function for finding the ideal function, which has the minimal sum
+     Input: sum deviation, min deviation
+     Output: list with yes if value is equal to min deviation, else no  """
+
+
+def find_ideal(sum_abw, min_abw):
+    result = []
+    for value in sum_abw:
+        if value == min_abw:
+            result.append("Yes")
+        else:
+            result.append("No")
+    return result
