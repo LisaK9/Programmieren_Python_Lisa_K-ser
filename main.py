b59c05eac18160708c401d7b921dfd350a6336ac
lisa0908 <108758315+lisa0908@users.noreply.github.com>
2023-01-09 12:03:35 +0100

    Berechnung der passenden Testdaten für FUnktion 1

--- main.py
+++ main.py
@@ -138,3 +138,10 @@ abw_test = calc_functions.least_squared(data_function1['y'], data_function1[
     column_name])  # calculate least square between y_test and y_ideal
 abw_train = data_function1['abw_train']  # get deviate from training data
 abw_test_train = abw_test - abw_train  # calculate deviate between test and train deviate
+data_function1['krit'] = calc_functions.value_krit(abw_train_max)  # add criterion to dataframe
+data_function1['abw_test'] = abw_test  # add abw_test to dataframe
+data_function1['abw_test_train'] = abw_test_train  # add sb_test_train to dataframe
+values = data_function1['abw_test_train'] < data_function1['krit']  # condition: abw test train has to be smaller than criterion
+data_function1 = data_function1[values]  # filter dataframe based on the condition
+data_function1['ideal_function'] = column_name  # add column_name of ideal function to dataframe
+print("testdata function1: ", data_function1)  # print testdata for function 1
