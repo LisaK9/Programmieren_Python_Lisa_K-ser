2655cf6b18353c6854492a9de6042bd71d1c37dd
lisa0908 <108758315+lisa0908@users.noreply.github.com>
2023-01-09 12:25:45 +0100

    Funktion zum plotten der idealen funktion und passenden testdaten

--- plot.py
+++ plot.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 
 plt.style.use('seaborn')
 
+"""plot function for the training data"""
+
 
 def train_functions(x, y1, y2, y3, y4):
     fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
@@ -14,5 +16,22 @@ def train_functions(x, y1, y2, y3, y4):
     ax3.set_title('train function 3')
     ax4.scatter(x, y4, s=8, color='blue')
     ax4.set_title('train function 4')
+    plt.tight_layout()
+    plt.show()
+
+
+"""plot function for the ideal function of the training data and the matching test data"""
+
+
+def plot_fit_test(x, y, y_ideal, label, x_test, y_test):
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    ax1.plot(x, y_ideal, color='red', linewidth=2, label='ideal function')
+    ax1.scatter(x, y, marker='o', color='blue', s=12, label=label)
+    ax1.set_title('train data and ideal function')
+    ax1.legend()
+    ax2.plot(x, y_ideal, color='red', linewidth=2, label='ideal_function')
+    ax2.scatter(x_test, y_test, color='black', marker="o", s=12, label='test data')
+    ax2.set_title('ideal function and matching test data')
+    ax2.legend()
     plt.tight_layout()
     plt.show()
